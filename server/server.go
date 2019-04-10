132f3d4b88a9c668f2e0c38eca8b95b754d457b4
Akio Yasui <akkyie01@gmail.com>
2019-04-10 22:12:25 +0900

    Implement ServerStreamingEcho

--- server/server.go
+++ server/server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"unicode/utf8"
 
 	"github.com/akkyie/grpc-echo/echo"
 )
@@ -25,8 +26,19 @@ func (server *Server) UnaryEcho(ctx context.Context, req *echo.EchoRequest) (*ec
 	return &echo.EchoResponse{Message: message}, nil
 }
 
-func (server *Server) ServerStreamingEcho(*echo.EchoRequest, echo.Echo_ServerStreamingEchoServer) error {
-	return fmt.Errorf("unimplemented")
+func (server *Server) ServerStreamingEcho(req *echo.EchoRequest, res echo.Echo_ServerStreamingEchoServer) error {
+	hostname, err := os.Hostname()
+	if err != nil {
+		return err
+	}
+
+	message := req.GetMessage()
+	response := &echo.EchoResponse{}
+	for i := 0; i <= utf8.RuneCountInString(message); i++ {
+		response.Message = fmt.Sprintf("[%s] %s", hostname, string([]rune(message)[:i]))
+		res.Send(response)
+	}
+	return nil
 }
 
 func (server *Server) ClientStreamingEcho(echo.Echo_ClientStreamingEchoServer) error {
